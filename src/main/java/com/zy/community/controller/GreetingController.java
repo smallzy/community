db347109e8c085095fda08a9e82c0563e82c9001
smallzy <953616590@qq.com>
2020-05-12 21:47:57 +0800

    init repo

---
+++ src/main/java/com/zy/community/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.zy.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * date：2020/5/12
+ * author：zy
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
